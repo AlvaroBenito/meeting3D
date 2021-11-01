833095dbeec3858f3e8c513453e3610ce11bffe4
AlvaroBenito <a.benitoo@alumnos.upm.es>
2021-11-01 16:00:14 +0100

    changed project name

---
+++ meeting3D/meeting3D/src/meeting3D.cpp
@@ -0,0 +1,15 @@
+// meeting3D.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
+using namespace std;
+
+void main() {
+    std::cout << "Hello World!\n";
+}
+
